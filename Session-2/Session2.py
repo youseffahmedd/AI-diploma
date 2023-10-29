f1c934cd066d497b6427bb9359b31554a9e3c88b
youseffahmedd <yousefff164@gmail.com>
2023-10-29 18:22:22 +0200

    .

---
+++ Session-2/Session2.py
@@ -0,0 +1,45 @@
+# task 1
+# num = int(input("enter a number : "))
+# print(abs(num))
+
+
+# task 2
+# year = int(input("enter a year : "))
+#
+# if year % 400 == 0 and year % 100 == 0:
+#     print(str(year) + " is a leap year")
+# elif year % 4 == 0 and year % 100 != 0:
+#     print(str(year) + " is a leap year")
+# else:
+#     print(str(year) + " is not leap year")
+
+
+# task 3
+# num1 = int(input("enter the first person age : "))
+# num2 = int(input("enter the second person age : "))
+# num3 = int(input("enter the third person age : "))
+#
+# if num1 >= num2 and num1 >= num3:
+#     print("First person is the oldest")
+#     if num2 > num3:
+#         print("Third person is the youngest")
+#     else:
+#         print("Second person is the youngest")
+#
+# elif num2 >= num1 and num2 >= num3:
+#     print("Second person is the oldest")
+#     if num1 > num3:
+#         print("Third person is the youngest")
+#     else:
+#         print("First person is the youngest")
+#
+# elif num3 >= num1 and num3 >= num2:
+#     print("Third person is the oldest")
+#     if num1 > num2:
+#         print("Second person is the youngest")
+#     else:
+#         print("First person is the youngest")
+
+
+
+
